554c3fb091b69792e86dbba84ccbd89fea66a8da
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-02-05 14:57:51 +0900

    add toppage template

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from '@chakra-ui/react'
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return <Container>JYUKUCASTER</Container>
+}
+
+export default Home
