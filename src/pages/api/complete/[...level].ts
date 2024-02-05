50c818a8442fbd113ad0a17f25034395882b6c5c
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-02-05 19:05:30 +0900

    get contract with abi

--- src/pages/api/complete/[...level].ts
+++ src/pages/api/complete/[...level].ts
@@ -9,7 +9,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (untrustedData.buttonIndex === 1) {
     res.redirect(302, `${process.env.NEXT_PUBLIC_SITE_URL}`)
   } else {
-    const tokenId = level === 'mecha' ? 2 : level === 'highschool' ? 1 : 0
+    const tokenId =
+      level?.toString() === 'mecha'
+        ? 2
+        : level?.toString() === 'highschool'
+        ? 1
+        : 0
     const openseaLink = `https://opensea.io/assets/base/${process.env.NFT_CONTRACT_ADDRESS}/${tokenId}`
     res.redirect(302, openseaLink)
   }
